151b8f49dc8843104592ff803397a1c1b30597bf
Fiskaa <dominic.naufal11@gmail.com>
2023-05-28 09:38:53 +0800

    First commit to upload to github

---
+++ webauthen/main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    path('sign-up', views.sign_up, name='sign_up'),
+    path('create-post', views.create_post, name='create_post'),
+    path('my-post', views.my_post, name='my_post'),
+]
